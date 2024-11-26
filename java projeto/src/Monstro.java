250f1d942435654a5880f6862dab28bacb52ea6a
Victor <victor.sales.ferreira.1@gmail.com>
2024-11-26 15:41:30 -0300

    Criando Classe Monstro

---
+++ java projeto/src/Monstro.java
@@ -0,0 +1,19 @@
+public class Monstro extends Personagem{
+    private String tipo; //Pra definir se ele é, por exmeplo, um lobo
+    private int XP;
+    public Monstro(String nome, String tipo, Level level, int vida, int XP){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.level = level;
+        this.XP = XP;
+        Vida = vida;
+    }
+
+    public Monstro(){}
+
+
+
+
+
+
+}
