31a8c03e9ac1a2da677551927632a50aacbcf543
nicol <nicolasrodriguesemery@gmail.com>
2024-11-30 16:30:47 -0300

    hmm comecei a main, sla

---
+++ java projeto/src/service/Main.java
@@ -0,0 +1,18 @@
+package service;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int x,y,z;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Editor de ficha");
+        System.out.println("-----------------");
+        System.out.println("O que deseja fazer?");
+        System.out.println("-");;
+
+
+    }
+}
--- java projeto/src/service/Monstro.java
+++ java projeto/src/service/Monstro.java
@@ -1,6 +1,8 @@
-package model;
+package service;
 
-public class Monstro extends Personagem{
+import model.Personagem;
+
+public class Monstro extends Personagem {
     private String tipo; //Pra definir se ele é, por exmeplo, um lobo
     private int XP;
     public Monstro(String nome, String tipo, Level level, int vida, int XP){
